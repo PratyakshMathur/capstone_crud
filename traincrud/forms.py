d9cc1deb98dda99c97371229f9508ab1e5ac1815
PratyakshMathur <pratyaksh.mathur28@gmail.com>
2021-04-29 14:49:48 +0530

    both working

--- traincrud/forms.py
+++ traincrud/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-from .models import Employee
+from .models import Train
 
 
 class Trainlog(forms.ModelForm):
 
     class Meta:
         model = Train
-        fields = __all__
-        
+        fields = '__all__'
+
+    def __init__(self,*args,**kwargs):
+        super(Trainlog,self).__init__(*args,**kwargs)
+        self.fields['Start'].empty_label = "Select" 
+        self.fields['End'].empty_label = "Select"
+
+
--- traincrud/views.py
+++ traincrud/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import Trainlog
+from .models import Train
 
 def train_list(request):
-    
-    return render(request,"traincrud/train_list.html")
+    context = {'train_list':Train.objects.all()}
+    return render(request,"traincrud/train_list.html",  context)
 
 
 def train_log(request):
-    return render(request,"traincrud/train_log.html")
-
+    if request.method == "GET":
+        log = Trainlog()
+        return render(request,"traincrud/train_log.html", {'log':log})
+    else:
+        log = Trainlog(request.POST)
+        if log.is_valid():
+            log.save()
+            return redirect('/list')
 
 def train_delete(request):
     return 
